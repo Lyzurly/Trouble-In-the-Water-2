ee9eda425be8c94d421fa78b95f8b9f426ee58ce
pierogigon <115884228+pierogigon@users.noreply.github.com>
2025-10-18 02:53:33 -0400

    fishing minigame v2
    added fish distance

--- Collaborator Stuff/Rogi's Stuff/Rogi's Scripts/fishing_manager.gd
+++ Collaborator Stuff/Rogi's Stuff/Rogi's Scripts/fishing_manager.gd
@@ -4,9 +4,14 @@ class_name FishingManager extends Node
 
 static var ref: FishingManager
 
+signal fishing_success
+signal fishing_failure_linebreak
+signal fishing_failure_escape
+
 enum FISHING_RESULTS {
 	SUCCESS,
-	FAIL
+	FAIL_LINEBREAK,
+	FAIL_ESCAPE
 }
 
 const ANGLER_1 = preload("uid://6rycvarcb1q")
@@ -25,7 +30,13 @@ const FISH_ONLY_PULL_STRENGTH: float = 0.05
 const BOTH_PULLING_STRENGTH: float = 1.5
 
 const MINIMUM_FISH_PULL_TIME: float = 1.0
-const MAXIMUM_FISH_PULL_TIME: float = 6.0
+const MAXIMUM_FISH_PULL_TIME: float = 3.5
+const EXTRA_PLAYER_PULL_TIME: float = 2.0
+const INITIAL_FISH_DISTANCE: float = 50.0
+const INITIAL_FISH_DISTANCE_MARGIN: float = 10.0
+
+const PLAYER_PULL_STRENGTH: float = 90.0
+const FISH_PULL_STRENGTH: float = 30.0
 
 var fish_array: Array[Fish] = [ANGLER_1,ANGLER_2,ANGLER_3,CLOWN_1,CLOWN_2,CLOWN_3,PUFFER_1,PUFFER_2,PUFFER_3]
 
@@ -45,8 +56,21 @@ var _line_tension_percent : float = 0:
 		if _line_tension_percent == val:
 			return
 		if val > 109.0:
-			end_fishing(FISHING_RESULTS.FAIL)
+			end_fishing(FISHING_RESULTS.FAIL_LINEBREAK)
+		if val < 1.0:
+			end_fishing(FISHING_RESULTS.FAIL_ESCAPE)
 		_line_tension_percent = clamp(val, 0.0, 110.0)
+
+var _fish_distance: float:
+	get:
+		return _fish_distance
+	set(val):
+		if _fish_distance == val:
+			return
+		if _fish_distance < 0.5:
+			end_fishing(FISHING_RESULTS.SUCCESS)
+			fishing_success.emit()
+		_fish_distance = clamp(val, 0.0, 200.0)
 
 var _current_fish: Fish:
 	get:
@@ -62,6 +86,7 @@ var _is_fish_pulling: bool = false:
 	set(val):
 		if _is_fish_pulling == val:
 			return
+		
 		_is_fish_pulling = val
 
 
@@ -83,6 +108,12 @@ func get_line_tension_percent():
 func set_line_tension_percent(percent: float) -> void:
 	_line_tension_percent = percent
 
+func get_fish_distance():
+	return _fish_distance
+
+func set_fish_distance(distance: float) -> void:
+	_fish_distance = distance
+
 func get_current_fish():
 	return _current_fish
 
@@ -95,19 +126,27 @@ func get_is_fish_pulling():
 func set_is_fish_pulling(truth_value: bool) -> void: 
 	_is_fish_pulling = truth_value
 
-func start_finshing() -> Fish:
+func start_finshing() -> void:
 	if !_is_fishing:
+		var random_fish: Fish = fish_array.pick_random()
+		set_current_fish(random_fish)
 		set_line_tension_percent(50.0)
+		set_fish_distance(randf_range(INITIAL_FISH_DISTANCE - INITIAL_FISH_DISTANCE_MARGIN, INITIAL_FISH_DISTANCE + INITIAL_FISH_DISTANCE_MARGIN))
 	set_is_fishing(true)
-	var random_fish: Fish = fish_array.pick_random()
-	set_current_fish(random_fish)
-	return random_fish
+
 
 func toggle_is_fish_pulling():
-	_is_fish_pulling = !_is_fish_pulling
-	await get_tree().create_timer(randf_range(MINIMUM_FISH_PULL_TIME, MAXIMUM_FISH_PULL_TIME)).timeout
-	toggle_is_fish_pulling()
-	print(_is_fish_pulling)
+	if _is_fish_pulling:
+		set_is_fish_pulling(false)
+		await get_tree().create_timer(EXTRA_PLAYER_PULL_TIME + randf_range(MINIMUM_FISH_PULL_TIME, MAXIMUM_FISH_PULL_TIME)).timeout
+		toggle_is_fish_pulling()
+	else:
+		set_is_fish_pulling(true)
+		await get_tree().create_timer(randf_range(MINIMUM_FISH_PULL_TIME, MAXIMUM_FISH_PULL_TIME)).timeout
+		toggle_is_fish_pulling()
+
+
+
 
 func update_line_tension(delta):
 #region booleans
@@ -124,13 +163,31 @@ func update_line_tension(delta):
 	else: #both pulling
 		_line_tension_percent += TENSION_INCREASE_RATE * _current_fish.size * delta * BOTH_PULLING_STRENGTH
 
+func update_fish_distance(delta):
+	#region booleans
+	var neither_pulling: bool = !Input.is_action_pressed("DebugFishing") and !get_is_fish_pulling()
+	var player_only_pulling: bool = Input.is_action_pressed("DebugFishing") and !get_is_fish_pulling()
+	var fish_only_pulling: bool = !Input.is_action_pressed("DebugFishing") and get_is_fish_pulling()
+	
+	if neither_pulling:
+		return
+	elif player_only_pulling:
+		_fish_distance -= PLAYER_PULL_STRENGTH * delta
+	elif fish_only_pulling:
+		_fish_distance +=  FISH_PULL_STRENGTH * delta
+	else:
+		_fish_distance -= (PLAYER_PULL_STRENGTH - FISH_PULL_STRENGTH) * delta
+
 func end_fishing(fishing_result: FISHING_RESULTS) -> void: 
 	set_is_fishing(false)
 	match fishing_result: 
 		FISHING_RESULTS.SUCCESS:
 			Globals.fish_collection.append(_current_fish)
-		FISHING_RESULTS.FAIL:
-			pass
+			fishing_success.emit()
+		FISHING_RESULTS.FAIL_LINEBREAK:
+			fishing_failure_linebreak.emit()
+		FISHING_RESULTS.FAIL_ESCAPE:
+			fishing_failure_escape.emit()
 
 func _process(delta):
 	if Input.is_action_just_pressed("DebugFishing"):
@@ -138,3 +195,4 @@ func _process(delta):
 	
 	if _is_fishing:
 		update_line_tension(delta)
+		update_fish_distance(delta)

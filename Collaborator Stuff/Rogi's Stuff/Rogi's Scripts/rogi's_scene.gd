71d84cf214cc7c7582688841a67f0910d305258d
pierogigon <115884228+pierogigon@users.noreply.github.com>
2025-10-25 05:26:13 -0400

    added fancy fishing bar, environment assets

--- Collaborator Stuff/Rogi's Stuff/Rogi's Scripts/rogi's_scene.gd
+++ Collaborator Stuff/Rogi's Stuff/Rogi's Scripts/rogi's_scene.gd
@@ -2,19 +2,36 @@ extends Node3D
 
 const ANGLER_1 = preload("uid://6rycvarcb1q")
 
-
+const FISH_DISPLAY_TIME: float = 1.0
+const FISH_SIZE_SCALAR = 0.3
 
 func _ready():
-	var fish_mesh: MeshInstance3D = $angler_mesh/Cube
-	var shader_material: ShaderMaterial = fish_mesh.mesh.surface_get_material(0) as ShaderMaterial
+	FishingManager.ref.fishing_success.connect(_on_fishing_success)
+	FishingManager.ref.fishing_failure_linebreak.connect(_on_fishing_failure_linebreak)
+	FishingManager.ref.fishing_failure_escape.connect(_on_fishing_failure_escape)
+
+func _on_fishing_success():
+	#get fish resource
+	var caught_fish: Fish = FishingManager.ref._current_fish
 	
-	shader_material.set_shader_parameter("hue_shift", ANGLER_1.hue_shift)
+	#instance fish scene and add to tree
+	var fish_instance: Node = caught_fish.fish_scene.instantiate()
+	add_child(fish_instance)
 	
+	#set fish scale
+	var fish_mesh: MeshInstance3D = fish_instance.get_child(0)
+	fish_mesh.scale = (1.0 + caught_fish.size * FISH_SIZE_SCALAR) * Vector3.ONE
 	
-	#var fish_instance: Node = ANGLER_1.fish_scene.instantiate()
-	#add_child(fish_instance)
-	#var fish_mesh: MeshInstance3D = fish_instance.get_child(0)
-	#var shader_material: ShaderMaterial = fish_mesh.mesh.surface_get_material(0) as ShaderMaterial
-#
-	#shader_material.set_shader_parameter("shader_parameter/shift_amount", ANGLER_1.hue_shift)
+	#set fish hue shift
+	var shader_material: ShaderMaterial = fish_mesh.get_surface_override_material(0) as ShaderMaterial
+	shader_material.set_shader_parameter("shift_amount", caught_fish.hue_shift)
 	
+	#despawn caught fish
+	await get_tree().create_timer(FISH_DISPLAY_TIME).timeout
+	fish_instance.queue_free()
+
+func _on_fishing_failure_linebreak():
+	pass
+
+func _on_fishing_failure_escape():
+	pass
